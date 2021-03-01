521989e0072a69adb2811b9c18e56d336fd4d8de
Thuy Bui <hoaxuyenchi2503@gmail.com>
2021-03-01 17:14:07 +0700

    update new code

--- InfectionLogAutomation/PageObject/LogEntry/LogEntryDetailPage.cs
+++ InfectionLogAutomation/PageObject/LogEntry/LogEntryDetailPage.cs
@@ -1,5 +1,6 @@
 ﻿using InfectionLogAutomation.PageObject.Common;
 using OpenQA.Selenium;
+using SeleniumCSharp.Core.DriverWrapper;
 using SeleniumCSharp.Core.ElementWrapper;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,7 @@ namespace InfectionLogAutomation.PageObject.LogEntry
         public readonly Button btnAdvancedSearch;
         public readonly ComboBox cbbInfectionType;
 
+        #region Actions
         public LogEntryDetailPage()
         {
             lblTitle = new Label(By.XPath("//div[@id=\"logForm\"]//h3"));
@@ -32,7 +34,35 @@ namespace InfectionLogAutomation.PageObject.LogEntry
             cbbEmployee = new ComboBox(By.Id("person"));
             btnAdvancedSearch = new Button(By.Id("advanceSearch"));
             cbbInfectionType = new ComboBox(By.Id("infectionType"));
-
         }
+
+        #endregion Actions
+
+        #region Check points
+        /// <summary>
+        /// Check to see if a form displays via form's title
+        /// </summary>
+        /// <param name="pageTitle"></param>
+        /// <returns></returns>
+        public bool CheckPageExist(string pageTitle)
+        {
+            DriverUtils.WaitForPageLoad();
+            string title = lblTitle.GetText();
+            return title.Contains(pageTitle);
+        }
+
+        /// <summary>
+        /// Check to see if UI of New Team Log entry display correctly
+        /// </summary>
+        /// <returns></returns>
+        public bool DoesUIDisplayCorrectly()
+        {
+            return txtRegion.IsDisplayed()
+                & txtCommunity.IsDisplayed()
+                & txtEmployee.IsDisplayed();
+                //& cbbInfectionType.IsDisplayed();                
+        }
+
+        #endregion Check points
     }
 }
